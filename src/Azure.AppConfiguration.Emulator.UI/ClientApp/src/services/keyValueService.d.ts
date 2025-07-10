45b8569b3b2bb03dde3fd536c656fb8296a38bb6
Zhiyuan Liang <zhiyuanliang@microsoft.com>
2025-07-10 15:35:08 +0800

    update

--- src/Azure.AppConfiguration.Emulator.UI/ClientApp/src/services/keyValueService.d.ts
+++ src/Azure.AppConfiguration.Emulator.UI/ClientApp/src/services/keyValueService.d.ts
@@ -3,11 +3,15 @@ interface KeyValueResponse {
     items: KeyValue[];
     '@nextLink'?: string;
 }
+interface KeysResponse {
+    items?: string[];
+    '@nextLink'?: string;
+}
 export declare const keyValueService: {
     getKeyValues: (keyFilter?: string, labelFilter?: string, nextLink?: string) => Promise<KeyValueResponse>;
     getKeyValue: (key: string, label?: string) => Promise<KeyValue | null>;
     createOrUpdateKeyValue: (key: string, request: KeyValueRequest, label?: string) => Promise<KeyValue | null>;
     deleteKeyValue: (kv: KeyValue) => Promise<boolean>;
-    getKeys: (nameFilter?: string) => Promise<string[]>;
+    getKeys: (nameFilter?: string, nextLink?: string) => Promise<KeysResponse>;
 };
 export {};
