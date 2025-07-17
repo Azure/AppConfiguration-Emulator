b6be358ce7104e61a960842f92f4c8f0ed486bcc
Zhiyuan Liang <zhiyuanliang@microsoft.com>
2025-07-17 17:41:29 +0800

    fix snake_case bug

--- src/Azure.AppConfiguration.Emulator.Service/Formatters/Json/KeyValueJsonInputFormatter.cs
+++ src/Azure.AppConfiguration.Emulator.Service/Formatters/Json/KeyValueJsonInputFormatter.cs
@@ -33,6 +33,7 @@ namespace Azure.AppConfiguration.Emulator.Service.Formatters.Json
         {
             return base.CanReadType(type) &&
                    (typeof(KeyValueFilterModel).IsAssignableFrom(type) ||
+                    typeof(KeyValueModel).IsAssignableFrom(type) ||
                     typeof(IEnumerable<KeyValue>).IsAssignableFrom(type));
         }
     }
