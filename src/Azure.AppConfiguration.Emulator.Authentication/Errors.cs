5fd573e64879dd307d27f054884d41f97a527061
Zhiyuan Liang <141655842+zhiyuanliang-ms@users.noreply.github.com>
2025-07-20 15:14:23 +0800

    Correct the challenge header when authentication method is not enabled (#28)
    * correct the challenge header when authentication method is not enabled
    
    * update error type
    
    * revert change

---
+++ src/Azure.AppConfiguration.Emulator.Authentication/Errors.cs
@@ -0,0 +1,12 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+namespace Microsoft.AppConfig.Service.Authentication
+{
+    public static class Errors
+    {
+        public const string InvalidCredential = "Invalid Credential";
+        public const string SchemeNotAllowed = "Authentication scheme disabled";
+        public const string Forbidden = "forbidden";
+    }
+}
