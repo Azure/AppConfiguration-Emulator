4fb91e3dc180915c0debeb80a82f4ea1a4067607
Zhiyuan Liang <141655842+zhiyuanliang-ms@users.noreply.github.com>
2025-07-29 10:22:30 +0800

    Revert "Add feature flag UI (#33)" (#37)
    This reverts commit 1ec3db610ed00e3913e95021a0fc5fbd0622d71d.

--- src/Azure.AppConfiguration.Emulator.UI/ClientApp/src/components/FeatureFlagList.d.ts
+++
@@ -1,7 +0,0 @@
-import { KeyValue } from '../models/keyValue';
-interface Props {
-    onCreate: () => void;
-    onEdit: (keyValue: KeyValue) => void;
-}
-export default function FeatureFlagList({ onCreate, onEdit }: Props): import("react/jsx-runtime").JSX.Element;
-export {};
--- src/Azure.AppConfiguration.Emulator.UI/ClientApp/src/utils/featureFlagUtils.d.ts
+++
@@ -1,8 +0,0 @@
-import { KeyValue } from '../models/keyValue';
-export declare const FEATURE_FLAG_PREFIX = ".appconfig.featureflag/";
-export declare const FEATURE_FLAG_CONTENT_TYPE = "application/vnd.microsoft.appconfig.ff+json;charset=utf-8";
-export declare function isFeatureFlag(keyValue: KeyValue): boolean;
-export declare function getFeatureFlagName(key: string): string;
-export declare function createFeatureFlagKey(name: string): string;
-export declare function parseFeatureFlagEnabled(value: string): boolean;
-export declare function createFeatureFlagValue(name: string, enabled: boolean): string;
