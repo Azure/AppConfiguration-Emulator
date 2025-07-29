fc0b8f6c20011dfb0b9de1839604e00e95ff866a
Zhiyuan Liang <141655842+zhiyuanliang-ms@users.noreply.github.com>
2025-07-29 10:22:13 +0800

    Revert "Add feature flag example (#34)" (#36)
    This reverts commit cddf9cab0fafed4edcc06835ce8dd8eb3e88d1fe.

--- examples/dotnet-sdk/Demo/Program.cs
+++ examples/dotnet-sdk/Demo/Program.cs
@@ -1,7 +1,5 @@
-﻿using Azure.Core;
-using Azure.Data.AppConfiguration;
+﻿using Azure.Data.AppConfiguration;
 using Microsoft.Extensions.Configuration;
-using Microsoft.FeatureManagement;
 using System;
 using System.Threading.Tasks;
 
@@ -21,12 +19,6 @@ namespace AppConfigurationEmulatorDemo
             var client = new ConfigurationClient(connectionString);
             await client.SetConfigurationSettingAsync(new ConfigurationSetting("testkey", "testvalue"));
 
-            // Create a feature flag
-            await client.SetConfigurationSettingAsync(
-                ".appconfig.featureflag/test-feature-flag",
-                "{\"id\":\"test-feature-flag\",\"enabled\":true}",
-                ContentType: "application/vnd.microsoft.appconfig.ff+json;charset=utf-8");
-
             // For information about Azure App Configuration provider, please go to: https://learn.microsoft.com/azure/azure-app-configuration/configuration-provider-overview
             var builder = new ConfigurationBuilder();
             builder.AddAzureAppConfiguration(options =>
@@ -38,11 +30,6 @@ namespace AppConfigurationEmulatorDemo
 
             string testValue = configuration["testkey"];
             Console.WriteLine($"Configuration setting for 'testkey': {testValue}");
-
-            var featureManager = new FeatureManager(
-                new ConfigurationFeatureDefinitionProvider(configuration));
-
-            Console.WriteLine($"'test-feature-flag': {await featureManager.IsEnabledAsync("test-feature-flag")}");
         }
     }
 }
---
+++ examples/javascript-sdk/demo.mjs
@@ -0,0 +1,28 @@
+const connectionString = process.env.APP_CONFIGURATION_EMULATOR_CONNECTION_STRING
+
+if (connectionString === undefined) {
+    throw new Error("APP_CONFIGURATION_EMULATOR_CONNECTION_STRING environment variable not found. Please run the start-emulator.ps1 script first.");
+}
+
+import { AppConfigurationClient } from "@azure/app-configuration";
+
+const client = new AppConfigurationClient(connectionString, { 
+    allowInsecureConnection: true // allow http connections for local emulator
+});
+
+await client.setConfigurationSetting({
+  key: "testkey",
+  value: "testvalue"
+});
+
+// For information about Azure App Configuration provider, please go to: https://learn.microsoft.com/azure/azure-app-configuration/configuration-provider-overview
+import {load } from "@azure/app-configuration-provider";
+
+const settings = await load(connectionString, {
+    clientOptions: {
+        allowInsecureConnection: true // allow http connections for local emulator
+    }
+});
+
+console.log(`Configuration setting for 'testkey': ${settings.get("testkey")}`);
+
